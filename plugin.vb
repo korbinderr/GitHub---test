23f9bc3a49652b86feb7a9325b21fb04199c27b5
korbinderr <73319984+korbinderr@users.noreply.github.com>
2020-11-10 19:07:14 -0700

    Create plugin.vb

---
+++ plugin.vb
@@ -0,0 +1,9 @@
+imports Systems
+Public Class Arrange
+  Dim stName As String
+  
+  Public Function Add(value As String) As String
+    Dim value as String
+    Console.WriteLine(value)
+  End Function
+End Class
